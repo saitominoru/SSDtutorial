d71efdc95369b5518a455962e5244a2d405c5e4e
saitominoru <x17c008c@mail.cc.niigata-u.ac.jp>
2020-12-23 17:56:55 +0900

    add new file

--- myNetwork/__init__.py
+++
@@ -1,7 +0,0 @@
-from .DBox import *
-from .L2Norm import *
-from .SSD import *
-from .make_vgg import *
-from .make_extras import *
-from .make_loc_conf import *
-
--- myUtilsData/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Anno_xml2list import *
-from .Dataloader import *
-from .DataTransform import *
-from .make_datapath_list import *
-from VOCDataset import *
